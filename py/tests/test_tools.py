142d6db295bb70d3107e81a19daaf7bc23167fd6
julien.pages <julien.pages@fa17faf6-23ee-8042-a9cf-75698c9d70c1>
2014-04-22 14:26:24 +0000

    ajout de tests
    git-svn-id: http://tsfe-svn/svn/logiciel/utilitaires_scle/test_ihm_qt/projet@113599 fa17faf6-23ee-8042-a9cf-75698c9d70c1

--- py/tests/test_tools.py
+++ py/tests/test_tools.py
@@ -1,6 +1,6 @@
 from nose.tools import *
 from funq import tools
-import time
+import time, sys, os
 
 def test_wait_for():
     def func():
@@ -24,3 +24,16 @@ def test_wait_for_some_time():
     def func():
         return t + 0.05 < time.time()
     assert_true(tools.wait_for(func, 0.1))
+
+def test_which():
+    assert_equals(sys.executable, tools.which(sys.executable))
+
+def test_which_with_pass():
+    path, fname = os.path.split(sys.executable)
+    old_env = os.environ
+    env = dict(PATH=path)
+    os.environ = env
+    try:
+        assert_equals(sys.executable, tools.which(fname))
+    finally:
+        os.environ = old_env
