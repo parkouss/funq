931203592ca1b544ce614eecf773874fb426751e
julien.pages <julien.pages@fa17faf6-23ee-8042-a9cf-75698c9d70c1>
2014-05-07 14:46:54 +0000

    ajout de tests objectPath
    git-svn-id: http://tsfe-svn/svn/logiciel/utilitaires_scle/test_ihm_qt/projet@114750 fa17faf6-23ee-8042-a9cf-75698c9d70c1

--- cpp/tests/libFunq/test.cpp
+++ cpp/tests/libFunq/test.cpp
@@ -1,5 +1,9 @@
 #include <QtTest/QtTest>
 #include <QObject>
+#include <QMainWindow>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
 #include "objectpath.h"
 
 class LibFunqTest: public QObject
@@ -18,6 +22,181 @@ private slots:
         
         QString name = ObjectPath::objectName(&obj);
         QCOMPARE(name, QString("NAMEd"));
+    }
+    void test_objectPath_objectName_noname_with_siblings() {
+        QObject parent;
+        QObject obj(&parent);
+        QObject obj2(&parent);
+        
+        QCOMPARE(ObjectPath::objectName(&obj), QString("QObject"));
+        QCOMPARE(ObjectPath::objectName(&obj2), QString("QObject-1"));
+    }
+    void test_objectPath_objectName_named_with_siblings() {
+        QObject parent;
+        QObject obj(&parent);
+        QObject obj2(&parent);
+        
+        obj.setObjectName("NAMEd");
+        obj2.setObjectName("NAMEd2");
+        
+        QCOMPARE(ObjectPath::objectName(&obj), QString("NAMEd"));
+        QCOMPARE(ObjectPath::objectName(&obj2), QString("NAMEd2"));
+    }
+    void test_objectPath_objectName_named_same_with_siblings() {
+        QObject parent;
+        QObject obj(&parent);
+        QObject obj2(&parent);
+        
+        obj.setObjectName("NAMEd");
+        obj2.setObjectName("NAMEd");
+        
+        QCOMPARE(ObjectPath::objectName(&obj), QString("NAMEd"));
+        QCOMPARE(ObjectPath::objectName(&obj2), QString("NAMEd-1"));
+    }
+    void test_objectPath_objectPath_simple() {
+        QObject parent;
+        QObject obj(&parent);
+        QObject obj2(&parent);
+        
+        obj.setObjectName("NAMEd");
+        obj2.setObjectName("NAMEd");
+        
+        QCOMPARE(ObjectPath::objectPath(&obj), QString("QObject::NAMEd"));
+        QCOMPARE(ObjectPath::objectPath(&obj2), QString("QObject::NAMEd-1"));
+    }
+    void test_objectPath_objectPath_simple_with_sep() {
+        QObject parent;
+        QObject obj(&parent);
+        QObject obj2(&parent);
+        
+        obj.setObjectName("::NAMEd");
+        obj2.setObjectName("NAMEd");
+        
+        QCOMPARE(ObjectPath::objectPath(&obj), QString("QObject:::_:NAMEd"));
+        QCOMPARE(ObjectPath::objectPath(&obj2), QString("QObject::NAMEd"));
+    }
+    void test_objectPath_findObject_simple() {
+        QMainWindow parent;
+        QObject obj(&parent);
+        QObject obj2(&parent);
+        
+        obj2.setObjectName("NAMEd");
+        
+        QCOMPARE(ObjectPath::findObject("QMainWindow::NAMEd"), &obj2);
+    }
+    void test_objectPath_findObject_with_sep() {
+        QMainWindow parent;
+        QObject obj(&parent);
+        QObject obj2(&parent);
+        
+        obj2.setObjectName("::NAMEd");
+        
+        QCOMPARE(ObjectPath::findObject("QMainWindow:::_:NAMEd"), &obj2);
+    }
+    void test_objectpath_graphicsItemPos_toplevel() {
+        QGraphicsView view;
+        QGraphicsScene scene;
+        view.setScene(&scene);
+        
+        QGraphicsRectItem item;
+        
+        scene.addItem(&item);
+        
+        QCOMPARE(ObjectPath::graphicsItemPos(&item), 0);
+    }
+    void test_objectpath_graphicsItemPos_multi_toplevel() {
+        QGraphicsView view;
+        QGraphicsScene scene;
+        view.setScene(&scene);
+        
+        QGraphicsRectItem item;
+        QGraphicsRectItem item2;
+        
+        scene.addItem(&item);
+        scene.addItem(&item2);
+        
+        item2.setZValue(-1200);
+        
+        QCOMPARE(ObjectPath::graphicsItemPos(&item), 0);
+        QCOMPARE(ObjectPath::graphicsItemPos(&item2), 1);
+    }
+    void test_objectpath_graphicsItemPos_child() {
+        QGraphicsView view;
+        QGraphicsScene scene;
+        view.setScene(&scene);
+        
+        QGraphicsRectItem parent;
+        QGraphicsRectItem item(&parent);
+        
+        scene.addItem(&parent);
+        
+        QCOMPARE(ObjectPath::graphicsItemPos(&item), 0);
+    }
+    void test_objectpath_graphicsItemPos_children() {
+        QGraphicsView view;
+        QGraphicsScene scene;
+        view.setScene(&scene);
+        
+        QGraphicsRectItem parent;
+        QGraphicsRectItem item(&parent);
+        QGraphicsRectItem item2(&parent);
+        
+        item.setZValue(-1);
+        item2.setZValue(0);
+        
+        scene.addItem(&parent);
+        
+        QCOMPARE(ObjectPath::graphicsItemPos(&item), 0);
+        QCOMPARE(ObjectPath::graphicsItemPos(&item2), 1);
+    }
+    void test_objectpath_graphicsItemPath_child() {
+        QGraphicsView view;
+        QGraphicsScene scene;
+        view.setScene(&scene);
+        
+        QGraphicsRectItem parent;
+        QGraphicsRectItem item(&parent);
+        
+        scene.addItem(&parent);
+        
+        QCOMPARE(ObjectPath::graphicsItemPath(&parent), QString("0"));
+        QCOMPARE(ObjectPath::graphicsItemPath(&item), QString("0/0"));
+    }
+    void test_objectpath_graphicsItemPath_children() {
+        QGraphicsView view;
+        QGraphicsScene scene;
+        view.setScene(&scene);
+        
+        QGraphicsRectItem parent;
+        QGraphicsRectItem item(&parent);
+        QGraphicsRectItem item2(&parent);
+        
+        item.setZValue(-1);
+        item2.setZValue(0);
+        
+        scene.addItem(&parent);
+        
+        QCOMPARE(ObjectPath::graphicsItemPath(&parent), QString("0"));
+        QCOMPARE(ObjectPath::graphicsItemPath(&item), QString("0/0"));
+        QCOMPARE(ObjectPath::graphicsItemPath(&item2), QString("0/1"));
+    }
+    void test_objectpath_graphicsItemFromPath() {
+        QGraphicsView view;
+        QGraphicsScene scene;
+        view.setScene(&scene);
+        
+        QGraphicsRectItem parent;
+        QGraphicsRectItem item(&parent);
+        QGraphicsRectItem item2(&parent);
+        
+        item.setZValue(-1);
+        item2.setZValue(0);
+        
+        scene.addItem(&parent);
+        
+        QCOMPARE(ObjectPath::graphicsItemFromPath(&view, "0"), &parent);
+        QCOMPARE(ObjectPath::graphicsItemFromPath(&view, "0/0"), &item);
+        QCOMPARE(ObjectPath::graphicsItemFromPath(&view, "0/1"), &item2);
     }
 };
 
