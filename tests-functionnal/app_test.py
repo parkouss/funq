292f0d204e93fd0302caf58b739b9e06e469b5d2
Julien Pagès <j.parkouss@gmail.com>
2015-04-11 13:56:04 +0200

    allow to use pyqt4 in functionnal tests

--- tests-functionnal/app_test.py
+++ tests-functionnal/app_test.py
@@ -32,7 +32,11 @@
 # The fact that you are presently reading this means that you have had
 # knowledge of the CeCILL v2.1 license and that you accept its terms.
 
-from PySide import QtGui, QtCore
+try:
+    from PyQt4 import QtGui, QtCore
+except ImportError:
+    from PySide import QtGui, QtCore
+
 import sys
 
 def exec_dlg(self, dlgclass):
